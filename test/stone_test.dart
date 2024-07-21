42371c10f2ee206529f428c74eeb16a8c53d6f3d
ReiRev <reirev2913@gmail.com>
2024-07-21 02:35:48 +0000

    remove unused packages

--- test/stone_test.dart
+++ test/stone_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gobo/stone.dart';
 
 void main() {
