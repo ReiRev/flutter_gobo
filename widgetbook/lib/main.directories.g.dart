df35b082727366400e599208ff2201c189043433
ReiRev <reirev2913@gmail.com>
2024-08-16 08:16:43 +0000

    update

--- widgetbook/lib/main.directories.g.dart
+++ widgetbook/lib/main.directories.g.dart
@@ -10,23 +10,15 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:widgetbook/widgetbook.dart' as _i1;
-import 'package:widgetbook_flutter_gobo/board.dart' as _i2;
-import 'package:widgetbook_flutter_gobo/intersection.dart' as _i3;
-import 'package:widgetbook_flutter_gobo/stone.dart' as _i4;
+import 'package:widgetbook_flutter_gobo/intersection.dart' as _i2;
+import 'package:widgetbook_flutter_gobo/stone.dart' as _i3;
 
 final directories = <_i1.WidgetbookNode>[
-  _i1.WidgetbookLeafComponent(
-    name: 'Board',
-    useCase: _i1.WidgetbookUseCase(
-      name: 'Board',
-      builder: _i2.buildBoardUseCase,
-    ),
-  ),
   _i1.WidgetbookLeafComponent(
     name: 'Intersection',
     useCase: _i1.WidgetbookUseCase(
       name: 'Intersection',
-      builder: _i3.buildIntersectionUseCase,
+      builder: _i2.buildIntersectionUseCase,
     ),
   ),
   _i1.WidgetbookComponent(
@@ -34,15 +26,15 @@ final directories = <_i1.WidgetbookNode>[
     useCases: [
       _i1.WidgetbookUseCase(
         name: 'Black Stone',
-        builder: _i4.buildBlackStoneUseCase,
+        builder: _i3.buildBlackStoneUseCase,
       ),
       _i1.WidgetbookUseCase(
         name: 'Empty Stone',
-        builder: _i4.buildEmptyStoneUseCase,
+        builder: _i3.buildEmptyStoneUseCase,
       ),
       _i1.WidgetbookUseCase(
         name: 'White Stone',
-        builder: _i4.buildWhiteStoneUseCase,
+        builder: _i3.buildWhiteStoneUseCase,
       ),
     ],
   ),
--- widgetbook/lib/stone.dart
+++ widgetbook/lib/stone.dart
@@ -1,54 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:gobo/gobo.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
-import 'package:gobo/stone.dart';
+import 'package:gobo/gobo.dart';
 import "./utils.dart";
 
 @widgetbook.UseCase(name: 'Empty Stone', type: Stone)
 Widget buildEmptyStoneUseCase(BuildContext context) {
-  return Stone(
-    radius: context.knobs.double
-        .slider(label: 'radius', initialValue: 50, min: 1, max: 1000),
-    onPressed: () {
-      popup(context, 'pressed');
-    },
-    onDoublePressed: () {
-      popup(context, 'double pressed');
-    },
+  return BoardConfig(
+    dimension: BoardDimension(
+      width: 500,
+      stoneRadius: context.knobs.double
+          .slider(label: 'radius', initialValue: 50, min: 1, max: 1000),
+    ),
+    child: EmptyStone(
+      onPressed: () {
+        popup(context, 'pressed');
+      },
+      onDoublePressed: () {
+        popup(context, 'double pressed');
+      },
+    ),
   );
 }
 
 @widgetbook.UseCase(name: 'Black Stone', type: Stone)
 Widget buildBlackStoneUseCase(BuildContext context) {
-  return Stone.black(
-    radius: context.knobs.double
-        .slider(label: 'radius', initialValue: 50, min: 1, max: 1000),
-    opacity: context.knobs.double
-        .slider(label: 'opacity', initialValue: 1, min: 0, max: 1),
-    onPressed: () {
-      popup(context, 'pressed');
-    },
-    onDoublePressed: () {
-      popup(context, 'double pressed');
-    },
+  return BoardConfig(
+    dimension: BoardDimension(
+      width: 500,
+      stoneRadius: context.knobs.double
+          .slider(label: 'radius', initialValue: 50, min: 1, max: 1000),
+    ),
+    child: BlackStone(
+      opacity: context.knobs.double
+          .slider(label: 'opacity', initialValue: 1, min: 0, max: 1),
+      onPressed: () {
+        popup(context, 'pressed');
+      },
+      onDoublePressed: () {
+        popup(context, 'double pressed');
+      },
+    ),
   );
 }
 
 @widgetbook.UseCase(name: 'White Stone', type: Stone)
 Widget buildWhiteStoneUseCase(BuildContext context) {
-  final Stone stone = Stone.white(
-    radius: context.knobs.double
-        .slider(label: 'radius', initialValue: 50, min: 1, max: 1000),
-    opacity: context.knobs.double
-        .slider(label: 'opacity', initialValue: 1, min: 0, max: 1),
-    onPressed: () {
-      popup(context, 'pressed');
-    },
-    onDoublePressed: () {
-      popup(context, 'double pressed');
-    },
+  return BoardConfig(
+    dimension: BoardDimension(
+      width: 500,
+      stoneRadius: context.knobs.double
+          .slider(label: 'radius', initialValue: 50, min: 1, max: 1000),
+    ),
+    child: WhiteStone(
+      opacity: context.knobs.double
+          .slider(label: 'opacity', initialValue: 1, min: 0, max: 1),
+      onPressed: () {
+        popup(context, 'pressed');
+      },
+      onDoublePressed: () {
+        popup(context, 'double pressed');
+      },
+    ),
   );
-  // change stone variant to black
-  return stone;
 }
