826d3c7b57d33b050303c2d813d2fdf581a34425
ReiRev <reirev2913@gmail.com>
2024-08-31 18:22:04 +0900

    apply fix

--- widgetbook/lib/board.dart
+++ widgetbook/lib/board.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
