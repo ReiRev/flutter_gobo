e08ca593a17d53bcfef21db8aac46b9de66d270d
ReiRev <reirev2913@gmail.com>
2024-09-08 14:30:22 +0900

    write test for stones

--- test/stone/stone_component_test.dart
+++ test/stone/stone_component_test.dart
@@ -1,17 +1,31 @@
+import 'package:flame/components.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flame_test/flame_test.dart';
 import 'package:gobo/gobo.dart';
 
 void main() {
-  final List<StoneComponent> stones = [
-    WikipediaBlackStone(),
-    WikipediaWhiteStone()
+  final List<StoneComponent Function()> stoneCreators = [
+    () => InvisibleStone(),
+    () => WikipediaBlackStone(),
+    () => WikipediaWhiteStone()
   ];
 
-  for (var stone in stones) {
-    group(stone.toString(), () {
+  for (var stoneCreator in stoneCreators) {
+    group(stoneCreator().toString(), () {
       test('should have a painter', () {
-        expect(stone.painter, isNotNull);
+        expect(stoneCreator().painter, isNotNull);
       });
+
+      testGolden(
+        'renders golden',
+        (game) async {
+          game.add(stoneCreator()
+            ..radius = 50
+            ..position = Vector2(50, 50));
+        },
+        size: Vector2(100, 100),
+        goldenFile: '_goldens/${stoneCreator().toString()}.png',
+      );
     });
   }
 }
