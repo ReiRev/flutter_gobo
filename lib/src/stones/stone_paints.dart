08e1231d76675d3a36e4ff17d627a95f0214fb22
ReiRev <reirev2913@gmail.com>
2024-09-15 23:33:52 +0900

    fix and format

--- lib/src/stones/stone_paints.dart
+++ lib/src/stones/stone_paints.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/painting.dart';
 import 'const.dart';
 
 abstract final class StonePainters {
